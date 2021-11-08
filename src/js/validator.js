327f34eb15569567762010fbdf76852d647cf566
Светлана <svetlanagreenfox@gmail.com>
2021-11-08 15:46:36 +0300

    Corrected regular expression

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,6 +1,6 @@
 export default class Validator {
   validateUsername(name) {
-    if (/^[^0-9_-][a-zA-Z-_]*\d{0,3}[^0-9_-]$/.test(name)) {
+    if (/^[^0-9_-][a-zA-Z-_]*\d{0,3}[a-zA-Z-_]*[^0-9_-]$/.test(name)) {
       this.name = name;
     } else {
       throw new Error('Некорректное имя пользователя!');
